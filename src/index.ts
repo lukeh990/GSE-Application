2a947af9a1dd657435a2716aaa3b160652697e0f
lukeh990 <lukeharding990@gmail.com>
2023-01-10 09:29:49 -0500

    Remove ESM and add PKG

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-import figlet from "figlet";
-import chalk from "chalk";
+import figlet = require("figlet");
+import chalk = require("chalk");
 console.log(chalk.green.bold(figlet.textSync("GSE Application", "Banner3")));
