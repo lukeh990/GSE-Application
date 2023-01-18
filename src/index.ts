3685552f94021f03f2853ec2e895115004a291e8
lukeh990 <lukeharding990@gmail.com>
2023-01-17 20:27:45 -0500

    Move enter key detection to new function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import figlet = require("figlet");
 import chalk = require("chalk");
+import { once } from "events";
 
 const stdin = process.stdin;
+const stdout = process.stdout;
 
 const lines = figlet
   .textSync("Luke Harding's\nGSE Application", "Big")
@@ -21,19 +23,26 @@ async function printHeader() {
       await timer(20);
     }
     process.stdout.write("\n");
-    await timer(700);
+    await timer(600);
   }
+}
+
+async function detectEnterKey() {
+  stdout.write("Press ENTER to continue\n");
+  stdin.setRawMode(true);
+  stdin.resume();
+  stdin.setEncoding("utf8");
+  while (true) {
+    const key: string = (await once(stdin, "data"))[0];
+    if (key === "\r") break;
+    if (key === "\u0003") process.exit(1);
+  }
+  stdin.setRawMode(false);
 }
 
 async function start() {
   await printHeader();
-  stdin.setRawMode(true);
-  stdin.resume();
-  stdin.setEncoding("utf8");
-  stdin.on("data", (key: string) => {
-    if (key === "\u0003") process.exit(1)
-    console.log(key);
-  });
+  await detectEnterKey();
 }
 
 start();
